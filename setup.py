ece915542cf9e87786ce29afe8c638e926861d92
hun0r <155096014+hun0r@users.noreply.github.com>
2024-06-19 15:43:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="e_kreta",
+    name="e-kreta-hun0r",
     version="0.1",
     author="hunor, ferivoq",
     description="A Python API for interacting with the eKreta system.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/hun0r/e-kreta",
+    url="https://github.com/hun0r/e-kreta-hun0r",
     packages=find_packages(),
     install_requires=[
         "requests",
