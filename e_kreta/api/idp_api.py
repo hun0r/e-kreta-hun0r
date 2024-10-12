7180e3409efab80e18a4cb5ac236d58caf98604e
hun0r <155096014+hun0r@users.noreply.github.com>
2024-10-12 23:33:43 +0200

    Update idp_api.py
    fixed stupid string breaking

--- e_kreta/api/idp_api.py
+++ e_kreta/api/idp_api.py
@@ -47,11 +47,11 @@ class IdpApiV1:
         url = "https://idp.e-kreta.hu/account/login"
 
         payload = f'ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fprompt%3Dlogin%26nonce%3DwylCrqT4oN6PPgQn2yQB0euKei9nJeZ6_ffJ-VpSKZU%26response_type%3Dcode%26code_challenge_method%3DS256%26scope%3Dopenid%2520email%2520offline_access%2520kreta-ellenorzo-webapi.public%2520kreta-eugyintezes-webapi.public%2520kreta-fileservice-webapi.public%2520kreta-mobile-global-webapi.public%2520kreta-dkt-webapi.public%2520kreta-ier-webapi.public%26code_challenge%3DHByZRRnPGb-Ko_wTI7ibIba1HQ6lor0ws4bcgReuYSQ%26redirect_uri%3Dhttps%253A%252F%252Fmobil.e-kreta.hu%252Fellenorzo-student%252Fprod%252Foauthredirect%26client_id%3Dkreta-ellenorzo-student-mobile-ios%26state%3Drefilc_student_mobile%26suppressed_prompt%3Dlogin&\
-        IsTemporaryLogin=False&\
-        UserName={UserName}&\
-        Password={Password}&\
-        InstituteCode={klik}&\
-        loginType=InstituteLogin&\
+IsTemporaryLogin=False&\
+UserName={UserName}&\
+Password={Password}&\
+InstituteCode={klik}&\
+loginType=InstituteLogin&\
         __RequestVerificationToken={__RequestVerificationToken}'
         headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
